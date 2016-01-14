528e2dfb45212d89aab6477bb1d151d30680f08f
Sean McCollum <anincrediblyshortname@gmail.com>
2016-01-14 13:29:45 -0800

    bash-install works, though i'm not sure if it does from curl like the readme says it will

--- readme.md
+++ readme.md
@@ -1,7 +1,32 @@
 ## Broccoli react skeleton
 Skeleton for react project using broccoli.
 
+### First make a project
+```
+mkdir my-new-project
+cd my-new-project
+npm init -y
+```
+You can use this in an existing project too, but it copies the Brocfile.js and app directories in this package, so you should back those up first.
+
+### Run the script from this package
+```
+cd my-new-project
+npm install -g broccoli-react-skeleton; broccoli-react-skeleton
+```
+Or if you're using mac, and maybe linux, use the bash install
+```
+cd my-new-project
+curl https://raw.githubusercontent.com/adiktofsugar/broccoli-react-skeleton/master/scripts/bash-install.sh | bash
+```
+If you want to invoke the command without actually being in the directory, then
+```
+curl https://raw.githubusercontent.com/adiktofsugar/broccoli-react-skeleton/master/scripts/bash-install.sh | bash -s -- path/to/project
+```
+
+## Contribute
+It would be awesome if you know of a better way to do this. I don't like globally installing things if I can help it and the local way isn't great.
+
 ## Todo
-- Make script to change the names to whatever the actual project should be
 - Add r.js to package it for production
 - Add uglifyJS to minify it for production
--- scripts/bash-install.sh
+++ scripts/bash-install.sh
@@ -1,33 +1,45 @@
 #!/bin/bash -eu
 set -o pipefail
-
-url="git://github.com/adiktofsugar/broccoli-react-skeleton.git"
+url="git@github.com:adiktofsugar/broccoli-react-skeleton.git"
+project_location=
 usage="
-bash-install [-h][-f][<url-to-project>]
+bash-install [-h][-u <url>][project-location]
   -h - help
-  <url-to-project> - git clone whatever url is passed (defaults to $url)
+  -u <url> - where the broccoli-react-skeleton is located. will git clone whatever url is passed (defaults to $url)
+  project-location - where your project is. defaults to the current directory
 Downloads the git repo and runs the install in the current directory
 "
 
-while getopts ":h" opt; do
+while getopts ":hu:" opt; do
     case "$opt" in
         h) echo "$usage"; exit;;
+        u) url="$OPTARG";;
     esac
 done
 shift $((OPTIND-1))
 if [[ -n "${1:-}" ]]; then
-    url="$1"
+    project_location="$1"
+fi
+if [[ -z "$project_location" ]]; then
+    project_location="$PWD"
 fi
 
-echo "url is $url"
+if ! [[ -e "$project_location/package.json" ]]; then
+    echo "$project_location/package.json not found." >&2
+    exit 1
+fi
 
+echo "url for repo is $url"
 download_path="$HOME/.broccoli-react-skeleton"
 if [[ -d "$download_path" ]]; then
     echo "..removing existing repo"
     rm -rf "$download_path"
 fi
+{ pushd .; } >/dev/null
 git clone "$url" "$download_path"
 cd "$download_path"
 npm install --production
+{ popd; } >/dev/null
+cd "$project_location"
 $download_path/scripts/install.js
 
